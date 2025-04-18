6a03e8d58d2ea10dd3edd02f996ad581e2ba79ad
Rivas <rivassec@users.noreply.github.com>
2025-04-17 21:32:30 -0700

    Recover pelicanconf.py and publishconf.py from gh-pages

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+AUTHOR = 'RivasSec'
+SITENAME = 'DevSecOps Notes'
+SITEURL = 'https://rivassec.github.io/devsecops-notes'
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("GitHub", "https://github.com/rivassec"),
+    ("LinkedIn", "https://linkedin.com/in/9082311s2"),  # Optional
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
